c4b4099bcb81b03ac2232ffd38f8aec111b9e3a7
minfan Chen <mingfan1202@gmail.com>
2024-11-14 19:43:33 +0800

    feat: React 實戰影音/加入 Actions

--- src/hexschool/todo-redux/src/slice/todosSlice.js
+++ src/hexschool/todo-redux/src/slice/todosSlice.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { createSlice } from '@reduxjs/toolkit';
 
+// 1.匯入 useDispatch
+// 2.建立的 actions 匯入
+
+
 export const todosSlice = createSlice({
   name: 'todos',
   initialState: [
     {
       id: 1,
-      text: '這是一段話很多很多話'
+      text: 'action.payload'
     }
-  ]
+  ],
+  reducers: {
+    // 狀態管理器
+    createTodo(state, action) {
+      console.log(state)
+      console.log(action)
+      state.push(action.payload)
+    }
+  }
 })
 
+// 定義 reducers 可以使用actions來匯出（具名匯出）
+export const { createTodo } = todosSlice.actions
 
 export default todosSlice.reducer
