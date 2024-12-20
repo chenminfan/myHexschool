7344984c2e64733eec35cb79ca0e2cf61cb7b7d8
minfan Chen <mingfan1202@gmail.com>
2024-12-21 00:16:49 +0800

    fix: 修正 路徑

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import ReactCraApp from './hexschool/react-cra/src/App';
+import ReactDailyApp from './hexschool/react-daily/App';
+
 
 
 export default function App() {
   return (
-    <>test</>
+    <Routes>
+      <Route path='/' element={<ReactCraApp />}>
+      </Route>
+      {/* 暫放 */}
+      <Route path='/reactDaily' element={<ReactDailyApp />}></Route>
+    </Routes>
   )
 }
--- src/hexschool/react-cra/.prettierrc.mjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "singleQuote": true,
-  "bracketSameLine": false,
-  "trailingComma": "es5",
-  "arrowParens": "always",
-  "printWidth": 100,
-  "semi": true,
-};
