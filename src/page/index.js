73527dabc84c2943eaca75f6260c4849b9e3a1dc
minfan Chen <mingfan1202@gmail.com>
2025-05-13 00:33:16 +0800

    fix: 調整專案目錄

--- src/page/index.js
+++ src/page/index.js
@@ -5,7 +5,7 @@ export default function PageIndex() {
   return (
     <div>
       <header className='d-flex justify-content-center py-2'>
-        <h3>專案練習</h3>
+        <h3>小專案</h3>
       </header>
       <main className='frontend'>
         <Outlet context={{}} />
